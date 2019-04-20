4e2d2963c3ce480b71bcfe1d83ca58ee660dabfc
Ivan <fl125@yandex.ru>
2019-04-21 02:36:53 +0300

    Update user entity

--- src/main/java/ru/romanov/durak/users/models/User.java
+++ src/main/java/ru/romanov/durak/users/models/User.java
@@ -1,59 +1,81 @@
 package ru.romanov.durak.users.models;
 
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.hibernate.annotations.Type;
+import lombok.Data;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
-import org.joda.time.DateTime;
 import org.springframework.data.domain.Persistable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.rememberme.PersistentRememberMeToken;
 
 import javax.persistence.*;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 
+@Data
 @Entity
-@Table(name="app_user")
-@JsonAutoDetect
+@Table(name = "\"user\"")
 public class User implements UserDetails, Serializable, Persistable<Long> {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
+    @NotEmpty(message = "{validation.username.notEmpty}")
+    @Size(min = 2, max = 25, message = "{validation.username.size}")
+    @Pattern(regexp = "[\\d\\w][\\d\\w\\s]*", message = "{validation.username.regexp}")
     private String username;
+
+    @JsonIgnore
+    @NotEmpty(message = "{validation.password.notEmpty}")
+    @Size(min = 2, max = 64, message = "{validation.password.size}")
+    @Column(name = "password", length = 64)
     private String password;
+
+    @Email(message = "{validation.email}")
     private String email;
+
+    @Size(max = 64, message = "{validation.firstname.size}")
+    @Pattern(regexp = "[а-яА-Яa-zA-Z]*", message = "{validation.firstname.regexp}")
     private String firstName;
+
+    @Size(max = 64, message = "{validation.lastname.size}")
+    @Pattern(regexp = "[а-яА-Яa-zA-Z]*", message = "{validation.lastname.regexp}")
     private String lastName;
+
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
     private Set<Role> roles = new HashSet<>();
-    private String about;
+
+    @Basic(fetch = FetchType.LAZY)
+    @Lob
     private byte[] photo;
+
+    private String about;
     private Integer wins = 0;
     private Integer loses = 0;
     private Integer totalGames = 0;
+    @DateTimeFormat(pattern = "dd MM yyyy")
     private Date birthDate;
+    @DateTimeFormat(pattern = "dd MM yyyy")
     private Date creationDate;
     private boolean enabled;
-    private int version;
-
-    @Version
-    @Column(name = "version")
-    public int getVersion() {
-        return version;
-    }
-    public void setVersion(int version) {
-        this.version = version;
-    }
 
     @Transient
     @Override
@@ -61,177 +83,33 @@ public class User implements UserDetails, Serializable, Persistable<Long> {
         return id == null;
     }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Enumerated(EnumType.STRING)
-    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
-    @Column(name = "role")
-    @ElementCollection(fetch = FetchType.EAGER)
-    @JsonIgnore
-    public Set<Role> getRoles() {
-        return roles;
-    }
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
-    @NotEmpty(message="{validation.username.notEmpty}")
-    @Size(min=2, max=25, message="{validation.username.size}")
-    @Pattern(regexp="[\\d\\w][\\d\\w\\s]*", message="{validation.username.regexp}")
-    @Column(name = "username", length = 25)
-    @Type(type = "string")
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @JsonIgnore
-    @NotEmpty(message="{validation.password.notEmpty}")
-    @Size(min=2, max=64, message="{validation.password.size}")
-    @Column(name = "password", length = 64)
-    @Type(type = "string")
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password=password;
-    }
-
-    @JsonIgnore
     @Override
-    @Column(name = "enabled")
-    public boolean isEnabled() {
-        return enabled;
-    }
-    public void setEnabled(boolean enabled){
-        this.enabled = enabled;
-    }
-
-    @Column(name = "email")
-    @Email(message="{validation.email}")
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Column(name = "first_name", length = 64)
-    @Size(max=64, message="{validation.firstname.size}")
-    @Pattern(regexp="[а-яА-Яa-zA-Z]*", message="{validation.firstname.regexp}")
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    @Column(name = "last_name", length = 64)
-    @Size(max=64, message="{validation.lastname.size}")
-    @Pattern(regexp="[а-яА-Яa-zA-Z]*", message="{validation.lastname.regexp}")
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @DateTimeFormat(pattern = "dd MM yyyy")
-    public Date getBirthDate() {
-        return birthDate;
-    }
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    @DateTimeFormat(pattern = "dd MM yyyy")
-    public Date getCreationDate() {
-        return creationDate;
-    }
-    public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public String getAbout() {
-        return about;
-    }
-    public void setAbout(String about) {
-        this.about = about;
-    }
-
-    @Basic(fetch= FetchType.LAZY)
-    @Lob
-    @Column(name = "photo")
-    public byte[] getPhoto() {
-        return photo;
-    }
-    public void setPhoto(byte[] photo) {
-        this.photo = photo;
-    }
-
-    @Column(name = "wins", length = 5)
-    public Integer getWins() {
-        return wins;
-    }
-    public void setWins(Integer wins) {
-        this.wins = wins;
-    }
-
-    @Column(name = "loses", length = 5)
-    public Integer getLoses() {
-        return loses;
-    }
-    public void setLoses(Integer loses) {
-        this.loses = loses;
-    }
-
-    @Column(name = "total_games", length = 5)
-    public Integer getTotalGames() {
-        return totalGames;
-    }
-    public void setTotalGames(Integer totalGames) {
-        this.totalGames = totalGames;
-    }
-
-    @JsonIgnore
     @Transient
-    @Override
+    @JsonIgnore
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<SimpleGrantedAuthority> result = new ArrayList<>();
-
-        for (Role role : getRoles()) {
-            result.add(new SimpleGrantedAuthority(role.getAuthority()));
-        }
-        return result;
+        return roles.stream()
+                .map(Role::name)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 
-    @JsonIgnore
-    @Transient
     @Override
+    @Transient
+    @JsonIgnore
     public boolean isAccountNonExpired() {
         return true;
     }
 
-    @JsonIgnore
-    @Transient
     @Override
+    @Transient
+    @JsonIgnore
     public boolean isAccountNonLocked() {
         return true;
     }
 
-    @JsonIgnore
-    @Transient
     @Override
+    @Transient
+    @JsonIgnore
     public boolean isCredentialsNonExpired() {
         return true;
     }
@@ -241,7 +119,7 @@ public class User implements UserDetails, Serializable, Persistable<Long> {
         String birthDateString = "";
         SimpleDateFormat format = new SimpleDateFormat("dd MM yyyy");
 
-        if (birthDate != null){
+        if (birthDate != null) {
             birthDateString = format.format(birthDate);
         }
 
@@ -253,7 +131,7 @@ public class User implements UserDetails, Serializable, Persistable<Long> {
         String creatingDateString = "";
         SimpleDateFormat format = new SimpleDateFormat("dd MM yyyy");
 
-        if (creationDate != null){
+        if (creationDate != null) {
             creatingDateString = format.format(creationDate);
         }
 
