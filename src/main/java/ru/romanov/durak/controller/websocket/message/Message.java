74909931d52744c7f5268041da93157b257c8013
Ivan <fl125@yandex.ru>
2019-06-10 03:08:53 +0300

    Add lombok annotations to message pojo

--- src/main/java/ru/romanov/durak/controller/websocket/message/Message.java
+++ src/main/java/ru/romanov/durak/controller/websocket/message/Message.java
@@ -2,9 +2,13 @@ package ru.romanov.durak.controller.websocket.message;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", defaultImpl = DefaultMessage.class, visible = true)
 @JsonSubTypes({
         @JsonSubTypes.Type(value = CardMessage.class, name = "SELECT_CARD")
