b83e885fa45f76f55fa5ca032eebba9b74dd85d4
Ivan <fl125@yandex.ru>
2019-08-16 03:32:06 +0300

    Add resource cache and VersionResourceResolver

--- src/main/java/ru/romanov/durak/configuration/WebAppConfig.java
+++ src/main/java/ru/romanov/durak/configuration/WebAppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.CacheControl;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
@@ -14,6 +15,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.resource.ContentVersionStrategy;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesView;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
@@ -25,6 +28,7 @@ import ru.romanov.durak.controller.websocket.SingleplayerWebSocket;
 import ru.romanov.durak.model.Lobby;
 
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 
 @Configuration
@@ -35,7 +39,14 @@ public class WebAppConfig extends WebMvcConfigurerAdapter implements WebSocketCo
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        VersionResourceResolver versionResourceResolver = new VersionResourceResolver()
+                .addVersionStrategy(new ContentVersionStrategy(), "/**");
+
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/")
+                .setCacheControl(CacheControl.maxAge(30, TimeUnit.DAYS))
+                .resourceChain(true)
+                .addResolver(versionResourceResolver);
     }
 
     @Override
