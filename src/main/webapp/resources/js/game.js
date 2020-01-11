340d5b3a13473efc7c136568f319d864cf18d154
Ivan <fl125@yandex.ru>
2020-01-11 20:30:22 +0300

    Card images preload refactoring

--- src/main/webapp/resources/js/game.js
+++ src/main/webapp/resources/js/game.js
@@ -1,50 +1,20 @@
-var images = new Map();
-var imageNames = [
-    "c0",
-    "c1",
-    "c2",
-    "c3",
-    "c4",
-    "c5",
-    "c6",
-    "c7",
-    "c8",
-    "d0",
-    "d1",
-    "d2",
-    "d3",
-    "d4",
-    "d5",
-    "d6",
-    "d7",
-    "d8",
-    "h0",
-    "h1",
-    "h2",
-    "h3",
-    "h4",
-    "h5",
-    "h6",
-    "h7",
-    "h8",
-    "s0",
-    "s1",
-    "s2",
-    "s3",
-    "s4",
-    "s5",
-    "s6",
-    "s7",
-    "s8",
-    "back"
-];
+const images = new Map();
 
 function preload() {
-    imageNames.forEach(function (imageName) {
-        var image = new Image();
-        image.src = "/resources/images/cards/" + imageName + ".png";
-        images.set(imageName, image);
-    });
+    const suits = ["c", "d", "h", "s"];
+    for (let i = 0; i < 9; i++) {
+        suits.forEach(function (suit) {
+            preloadCardImage(suit + i);
+        })
+    }
+
+    preloadCardImage("back");
+}
+
+function preloadCardImage(imageName) {
+    let image = new Image();
+    image.src = "/resources/images/cards/" + imageName + ".png";
+    images.set(imageName, image);
 }
 
 function hideButtons() {
