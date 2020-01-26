4f22647a26b2c4708c9809c83d668cbb57437b9c
Ivan <fl125@yandex.ru>
2020-01-26 17:18:04 +0300

    Add lombok to json helper

--- src/main/java/ru/romanov/durak/util/JsonHelper.java
+++ src/main/java/ru/romanov/durak/util/JsonHelper.java
@@ -2,32 +2,28 @@ package ru.romanov.durak.util;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
 
-import java.io.IOException;
-
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class JsonHelper {
 
-    private final static ObjectMapper MAPPER;
+    private static final ObjectMapper MAPPER;
 
     static {
         MAPPER = new ObjectMapper();
         MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
+    @SneakyThrows
     public static <T> T parseJson(String json, Class<T> type) {
-        try {
-            return MAPPER.readValue(json, type);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return MAPPER.readValue(json, type);
     }
 
+    @SneakyThrows
     public static String convertObject(Object data) {
-        try {
-            return MAPPER.writeValueAsString(data);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return MAPPER.writeValueAsString(data);
     }
 
 }
