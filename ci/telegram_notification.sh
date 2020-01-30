4a1713fb949c19c65843ef532f3ccc0f89bfc568
Ivan <fl125@yandex.ru>
2020-01-31 02:47:48 +0300

    Add ci telegram notification

---
+++ ci/telegram_notification.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ $TRAVIS_TEST_RESULT -eq 0 ]; then
+    build_status="✅"
+else
+    build_status="❌"
+fi
+
+BOT_URL="https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage"
+
+send_msg () {
+    curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
+        -d text="$1" -d parse_mode=Markdown
+}
+
+send_msg="*${TRAVIS_REPO_SLUG}*
+
+Status: ${build_status}
+Branch: ${TRAVIS_BRANCH}
+Message:
+${TRAVIS_COMMIT_MESSAGE}
+
+[Job Log](${TRAVIS_JOB_WEB_URL})"
