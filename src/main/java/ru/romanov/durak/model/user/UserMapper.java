d7665d47c7f95e8a9c2b3bedf07315f4310c855f
Ivan <fl125@yandex.ru>
2020-02-02 23:38:59 +0300

    Ignore mapstruct warnings

--- src/main/java/ru/romanov/durak/model/user/UserMapper.java
+++ src/main/java/ru/romanov/durak/model/user/UserMapper.java
@@ -2,10 +2,11 @@ package ru.romanov.durak.model.user;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 import ru.romanov.durak.model.user.dto.UserDto;
 
-@Mapper
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
