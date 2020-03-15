a2bf353ba1d4a1a0efcac6683f3d23df7e47baa4
Ivan <fl125@yandex.ru>
2020-03-15 22:00:23 +0300

    Move resource configuration to application.yml

--- src/main/java/ru/romanov/durak/config/WebConfig.java
+++ src/main/java/ru/romanov/durak/config/WebConfig.java
@@ -3,44 +3,22 @@ package ru.romanov.durak.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.http.CacheControl;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.resource.ContentVersionStrategy;
-import org.springframework.web.servlet.resource.VersionResourceResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesView;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 import java.util.Locale;
-import java.util.concurrent.TimeUnit;
-
 
 @Configuration
-@EnableWebMvc
-@EnableWebSocket
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        VersionResourceResolver versionResourceResolver = new VersionResourceResolver()
-                .addVersionStrategy(new ContentVersionStrategy(), "/**");
-
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/")
-                .setCacheControl(CacheControl.maxAge(30, TimeUnit.DAYS))
-                .resourceChain(true)
-                .addResolver(versionResourceResolver);
-    }
 
     @Bean
     public ViewResolver tilesViewResolver() {
