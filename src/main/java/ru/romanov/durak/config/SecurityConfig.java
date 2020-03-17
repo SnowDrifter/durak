07708afa65aba520ca201e5be92c7ff0701425d5
Ivan <fl125@yandex.ru>
2020-03-17 22:43:27 +0300

    Refactoring security config

--- src/main/java/ru/romanov/durak/config/SecurityConfig.java
+++ src/main/java/ru/romanov/durak/config/SecurityConfig.java
@@ -4,7 +4,6 @@ package ru.romanov.durak.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
-import ru.romanov.durak.user.service.UserService;
 
 import javax.sql.DataSource;
 
@@ -22,19 +20,10 @@ import javax.sql.DataSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private UserService userService;
-    @Autowired
     private DataSource dataSource;
-
-    @Autowired
-    public void configureGlobalSecurity(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.userDetailsService(userService);
-
         http.formLogin()
                 .loginPage("/login")
                 .failureUrl("/login?error=true")
@@ -43,8 +32,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.logout().logoutSuccessUrl("/");
 
         http.authorizeRequests()
-                .antMatchers("/multiplayer", "/edit").access("isAuthenticated()")
-                .antMatchers("/static/**").permitAll();
+                .antMatchers("/multiplayer", "/edit").authenticated()
+                .anyRequest().permitAll();
 
         http.csrf().disable();
 
