c7980a830ae9dda6f5a6a05eb8fb40e16f40061e
Ivan <fl125@yandex.ru>
2020-03-20 00:01:47 +0300

    Update database config

--- src/main/java/ru/romanov/durak/config/DatabaseConfig.java
+++ src/main/java/ru/romanov/durak/config/DatabaseConfig.java
@@ -1,12 +1,34 @@
 package ru.romanov.durak.config;
 
+import org.jooq.impl.DataSourceConnectionProvider;
+import org.jooq.impl.DefaultConfiguration;
+import org.jooq.impl.DefaultDSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
 
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackages = {"ru.romanov.durak"})
 public class DatabaseConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public DataSourceConnectionProvider connectionProvider() {
+        return new DataSourceConnectionProvider(new TransactionAwareDataSourceProxy(dataSource));
+    }
+
+    @Bean
+    public DefaultDSLContext context() {
+        DefaultConfiguration configuration = new DefaultConfiguration();
+        configuration.set(connectionProvider());
+        return new DefaultDSLContext(configuration);
+    }
 
 }
