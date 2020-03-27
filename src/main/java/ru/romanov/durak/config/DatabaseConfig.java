5aaaa5b8acee8032df243b4d0f95ddce5a1b00e9
Ivan <fl125@yandex.ru>
2020-03-27 22:33:18 +0300

    Add ExceptionTranslator

--- src/main/java/ru/romanov/durak/config/DatabaseConfig.java
+++ src/main/java/ru/romanov/durak/config/DatabaseConfig.java
@@ -1,23 +1,25 @@
 package ru.romanov.durak.config;
 
+import lombok.RequiredArgsConstructor;
 import org.jooq.impl.DataSourceConnectionProvider;
 import org.jooq.impl.DefaultConfiguration;
 import org.jooq.impl.DefaultDSLContext;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.jooq.impl.DefaultExecuteListenerProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import ru.romanov.durak.util.ExceptionTranslator;
 
 import javax.sql.DataSource;
 
 
 @Configuration
+@RequiredArgsConstructor
 @EnableTransactionManagement
 public class DatabaseConfig {
 
-    @Autowired
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
     @Bean
     public DataSourceConnectionProvider connectionProvider() {
@@ -28,7 +30,13 @@ public class DatabaseConfig {
     public DefaultDSLContext context() {
         DefaultConfiguration configuration = new DefaultConfiguration();
         configuration.set(connectionProvider());
+        configuration.set(new DefaultExecuteListenerProvider(exceptionTranslator()));
         return new DefaultDSLContext(configuration);
+    }
+
+    @Bean
+    public ExceptionTranslator exceptionTranslator() {
+        return new ExceptionTranslator();
     }
 
 }
