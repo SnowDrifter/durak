03d402a9c57a5b01b6549b22af2e95b413abe3f7
Ivan <fl125@yandex.ru>
2020-04-12 16:53:44 +0300

    Add user service to basic security config

--- src/main/java/ru/romanov/durak/config/SecurityConfig.java
+++ src/main/java/ru/romanov/durak/config/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.userDetailsService(userService);
+
         http.formLogin()
                 .loginPage("/login")
                 .failureUrl("/login?error=true")
